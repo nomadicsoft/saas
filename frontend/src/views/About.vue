ced53555cb9e83c7eadecfca5e6bbe0bd51ebd52
kurumbus <grigory.shein@gmail.com>
2020-12-20 15:58:09 +0300

    bit of cleaning

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <v-container>
+        <v-row class="text-center">
+            <v-col cols="12">
+                <router-link :to="{name: 'home'}">Home</router-link>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
+
+<script>
+    export default {
+        name: 'about',
+    }
+</script>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <v-container>
+        <v-row class="text-center">
+            <v-col cols="12">
+                <router-link :to="{name: 'about'}">About</router-link>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+
   }
 }
 </script>
