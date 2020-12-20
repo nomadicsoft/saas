d7bb824c98f292321a50f6950cd4f1d1ca904b41
kurumbus <grigory.shein@gmail.com>
2020-12-20 15:21:17 +0300

    add vuetify

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,18 +1,16 @@
 module.exports = {
-    devServer: {
-        proxy: 'http://saas.local'
+    "devServer": {
+        "proxy": "http://saas.local"
     },
-
-    // output built static files to Laravel's public dir.
-    // note the "build" script in package.json needs to be modified as well.
-    outputDir: '../public/assets/',
-
-    publicPath: process.env.NODE_ENV === 'production'
+    "outputDir": "../public/assets/",
+    "publicPath": process.env.NODE_ENV === 'production'
         ? '/assets/'
         : '/',
-
     // modify the location of the generated HTML file.
-    indexPath: process.env.NODE_ENV === 'production'
+    "indexPath": process.env.NODE_ENV === 'production'
         ? '../../resources/views/spa.blade.php'
-        : 'index.html'
+        : 'index.html',
+    "transpileDependencies": [
+        "vuetify"
+    ]
 }
