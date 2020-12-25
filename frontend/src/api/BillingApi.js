639ca2d1957f76b48e66a356783235bb8708dd9a
kurumbus <grigory.shein@gmail.com>
2020-12-25 20:17:29 +0300

    rejecting promises

--- frontend/src/api/BillingApi.js
+++ frontend/src/api/BillingApi.js
@@ -1,23 +1,25 @@
 export default {
     loadIntentToken(){
-        return new Promise((resolve) => {
-            window.axios.get('/api/billing/setup-intent').then(response => resolve(response.data))
+        return new Promise((resolve,reject) => {
+            window.axios.get('/api/billing/setup-intent')
+                        .then(response => resolve(response.data))
+                        .catch(e => reject(e));
         })
     },
 
     updateSubscription(paymentMethodStripeId, planStripeId){
-        return new Promise((resolve) => {
+        return new Promise((resolve,reject) => {
             window.axios.put('/api/billing/subscription', {
                 plan: planStripeId,
                 payment: paymentMethodStripeId
-            }).then(res => resolve(res.data) );
+            }).then(res => resolve(res.data) ).catch(e => reject(e));
         })
     },
     removePaymentMethod( methodStripeId ) {
-        return new Promise((resolve) => {
+        return new Promise((resolve, reject) => {
             window.axios.post('/api/billing/remove-payment', {
                 id: methodStripeId
-            }).then(res => resolve(res.data));
+            }).then(res => resolve(res.data)).catch(e => reject(e));
         })
     }
 
