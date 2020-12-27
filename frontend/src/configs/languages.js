35f72fa897fa599037acf429c94145fdf0f0bb26
kurumbus <grigory.shein@gmail.com>
2020-12-27 11:42:33 +0300

    add available languages

---
+++ frontend/src/configs/languages.js
@@ -0,0 +1,52 @@
+export default [
+    {
+        code: 'en',
+        flag: 'us',
+        label: 'English',
+    },
+    {
+        code: 'es',
+        flag: 'es',
+        label: 'Español',
+    },
+    {
+        code: 'pt',
+        flag: 'pt',
+        label: 'Português',
+    },
+    {
+        code: 'de',
+        flag: 'de',
+        label: 'Deutsche',
+    },
+    {
+        code: 'fr',
+        flag: 'fr',
+        label: 'Français',
+    },
+    {
+        code: 'ar',
+        flag: 'sa',
+        label: 'العربية',
+    },
+    {
+        code: 'ko',
+        flag: 'kr',
+        label: '한국어',
+    },
+    {
+        code: 'ru',
+        flag: 'ru',
+        label: 'русский',
+    },
+    {
+        code: 'zh',
+        flag: 'cn',
+        label: '中文',
+    },
+    {
+        code: 'ja',
+        flag: 'jp',
+        label: '日本語',
+    }
+]
